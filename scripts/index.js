13dde1038c510705b2615008e3f383321c6296a7
JordanMRutherford <JordanMRutherford@gmail.com>
2022-08-08 12:54:11 -0400

    image fix attempt 8

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ let i;
 for (let i = (stores.length - 1); i >= (stores.length - 5); i--) {
   const storeDescription = `
   <div class="home_listing" style="outline: solid 2px #059669; border-radius: 0.25rem; margin-bottom:1rem">
-    <img class="home_listing_img" src=${stores[i].properties.imageAddress} style="height: 100%; float: left;">
+    <img class="home_listing_img" src="${stores[i].properties.imageAddress}" style="height: 100%; float: left;">
     <div class="home_listing_text" style="display: inline-block; vertical-align: middle; line-height: double;"> 
       <div style="font-weight: bold">${stores[i].properties.name}</div> 
       <div>${stores[i].properties.phoneNumber}</div> 
